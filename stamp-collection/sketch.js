6861112f7284795ba26269665e45d9a46919f5ab
Nicklas Kenyon <nick@kinoteksoftware.com>
2023-07-10 12:19:18 -0400

    Switch to chat driven stamping

--- stamp-collection/sketch.js
+++ stamp-collection/sketch.js
@@ -8,7 +8,9 @@ var xRadius;
 var yRadius;
 var delay = 0.05;
 var timer = 0;
-const shrinkPerSecond = 64; 
+const shrinkPerSecond = 64;
+
+const chatImages = [];
 
 function setup() {
     createCanvas(1920, 1080);
@@ -16,8 +18,8 @@ function setup() {
     angleMode(DEGREES);
     imageMode(CENTER);
 
-    xRadius = width / 2 + 200;
-    yRadius = width / 2 + 200;
+    xRadius = width / 2 - 200;
+    yRadius = height / 2 - 200;
 
     images = [
         loadImage(`${imageRoot}/Aliens.png`),
@@ -34,16 +36,13 @@ function setup() {
     ];
 }
 
-
-
-function draw() {
+function randomFill () {
     if(timer > delay && xRadius > 32) {
         const angle = Math.random() * 2 * Math.PI;
         const x =  xRadius * Math.sin(angle) + width / 2;
         const y = yRadius * Math.cos(angle) + height / 2;
         const index = Math.round(Math.random() * (images.length - 1));
         const img = images[index];
-        console.log(index, images.length, img);
         image(img, x, y);
         timer = 0;
         xRadius -= (1 * delay) * shrinkPerSecond;
@@ -52,3 +51,26 @@ function draw() {
         timer += deltaTime;
     }   
 }
+
+function draw() {
+    clear();
+    background('rgba(0,0,0,0.0)');
+    chatImages.forEach(({ img, x, y }) => {
+        image(img, x, y);
+    }) 
+}
+
+ComfyJS.onChat = (user, message, flags, self, extra) => {
+    if(message.slice(0, 5) === "stamp") {
+        const index = Math.round(Math.random() * (images.length - 1));
+        const img = images[index];
+        const angle = Math.random() * 2 * Math.PI;
+        const x =  xRadius * Math.sin(angle) + width / 2;
+        const y = yRadius * Math.cos(angle) + height / 2;
+        xRadius -= (1 * delay) * shrinkPerSecond;
+        yRadius -= (1 * delay) * shrinkPerSecond;
+        chatImages.push({ img, x, y });
+    }
+};
+
+ComfyJS.Init("Johnny_Boombotz");
